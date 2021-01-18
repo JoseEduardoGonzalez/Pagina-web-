8aaf4ca59cb8812a870987a4ab73165a92735747
JoseEduardoGonzalez <77603272+JoseEduardoGonzalez@users.noreply.github.com>
2021-01-17 23:34:26 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,274 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="description" content="Coca cola la bebida mas sabroda y adicta del mundo ">
+		<meta name="Keywords" content="bebida, gaseosa, cola">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>
+			Inicio-Coca cola
+
+		</title>
+		<link rel="shortcut icon" href="img/favicon.png">
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
+		<link rel="stylesheet" href="css/normalize.css">
+		<link rel="stylesheet" href="style.css">
+		<script src="https://kit.fontawesome.com/599167b72c.js" crossorigin="anonymous"></script>
+	</head>
+	<body>
+		<header>
+			<div class="container">
+				<a href="index.html">
+					<img src="img/logo.svg" width="100px">
+				</a>
+				<nav>
+					<a href="#inicio">Inicio</a>
+					<a href="#nosotros">Nosotros</a>
+					<a href="#servicios">Servicios</a>
+					<a href="#galeria">Galeria</a>
+					<a href="#contactenos">Contactenos</a>
+				</nav>
+				<a href="#" class="hamb"><i class="fas fa-bars"></i></a>
+			</div>
+		</header>
+		<main>
+			<section id="inicio">
+				<img src="img/bannerprincipal.jpg">
+				<div class="bloque-inicio">
+					<h1>Bienvenidos a Coca Cola</h1>
+					<P>
+				    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur tincidunt augue eget ipsum porta, sit amet fermentum eros malesuada. 
+					</P>
+					<a href="#nosotros" class="boton boton-rojo">Ver más</a>
+				</div>
+
+			</section>
+			<section id="nosotros" class="seccion">
+				<div class="container">
+					<p>Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Ut tincidunt posuere condimentum. Quisque pellentesque ultricies turpis, a efficitur lacus lacinia quis. Sed accumsan dignissim facilisis.
+					</p>
+				</div>
+
+			</section>
+			<section id="servicios" class="seccion">
+				<div class="container">
+					<div class="row">
+						<div class="columna columna-33 columna-mobile-100">
+							<div class="bloque-servicio">
+								<div class="bloque-img-servicio cuadrado-perfecto">
+									<img src="img/servicio1.jpg">
+								</div>
+								<div class="bloque-contenido-servicio">
+									<h3>Servicio 1</h3>
+									<p>
+									Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Ut tincidunt posuere condimentum. Quisque pellentesque ultricies turpis, a efficitur lacus lacinia quis. Sed accumsan dignissim facilisis.
+									</p>
+									<a href="#" class="boton boton-blanco">Ver más</a>
+									
+								</div>
+								
+							</div>
+						</div>
+
+						<div class="columna columna-33 columna-mobile-100">
+							<div class="bloque-servicio">
+								<div class="bloque-img-servicio cuadrado-perfecto">
+									<img src="img/servicio2.jpg">
+								</div>
+								<div class="bloque-contenido-servicio">
+									<h3>Servicio 2</h3>
+									<p>
+									Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Ut tincidunt posuere condimentum. Quisque pellentesque ultricies turpis, a efficitur lacus lacinia quis. Sed accumsan dignissim facilisis.
+									</p>
+									<a href="#" class="boton boton-blanco">Ver más</a>
+									
+								</div>
+								
+							</div>
+						</div>
+
+						<div class="columna columna-33 columna-mobile-100">
+							<div class="bloque-servicio">
+								<div class="bloque-img-servicio cuadrado-perfecto">
+									<img src="img/servicio3.jpg">
+								</div>
+								<div class="bloque-contenido-servicio">
+									<h3>Servicio 3</h3>
+									<p>
+									Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Ut tincidunt posuere condimentum. Quisque pellentesque ultricies turpis, a efficitur lacus lacinia quis. Sed accumsan dignissim facilisis.
+									</p>
+									<a href="#" class="boton boton-blanco">Ver más</a>
+									
+								</div>
+								
+							</div>
+						</div>
+							
+					</div>
+					
+				</div>
+
+			</section>
+			<section id="galeria">
+				<div class="container-fluid">
+					<div class="row">
+						<div class="columna-33 columna-mobile-50">
+							<div class="cuadrado-perfecto">
+								<img src="img/servicio1.jpg">
+								<h4>Imagen 1</h4>
+							</div>
+						</div>
+
+						<div class="columna-33 columna-mobile-50">
+							<div class="cuadrado-perfecto">
+								<img src="img/servicio2.jpg">
+								<h4>Imagen 2</h4>
+							</div>
+
+						</div>
+
+						<div class="columna-33 columna-mobile-50">	
+							<div class="cuadrado-perfecto">
+								<img src="img/servicio1.jpg">
+								<h4>Imagen 3</h4>
+							</div>
+						</div>
+
+						<div class="columna-33 columna-mobile-50">	
+							<div class="cuadrado-perfecto">
+								<img src="img/servicio1.jpg">
+								<h4>Imagen 4</h4>
+							</div>
+						</div>
+
+						<div class="columna-33 columna-mobile-50">
+							<div class="cuadrado-perfecto">
+								<img src="img/servicio2.jpg">
+								<h4>Imagen 5</h4>
+							</div>
+						</div>
+
+						<div class="columna-33 columna-mobile-50">
+							<div class="cuadrado-perfecto">
+								<img src="img/servicio3.jpg">
+								<h4>Imagen 6</h4>
+							</div>
+
+						</div>
+
+					</div>
+					
+				</div>
+
+			</section>
+			<section id="contactenos" class="seccion">
+				<iframe width="700" height="440" src="https://maps.google.com/maps?width=700&amp;height=440&amp;hl=en&amp;q=Madrid%2C%20Spain+(ciudad%20de%20mexico)&amp;ie=UTF8&amp;t=&amp;z=10&amp;iwloc=B&amp;output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+				<div class="container">
+					<div class="row">
+						<div class="columna columna-41 columna-mobile-100 empujar-66 empujar-mobile-0 sinpadding-mobile">
+							<form action="index.php" method="POST">
+								<div class="form-block">
+									<input type="text" name="nombre" class="form-control" placeholder="Nombre">
+									
+								</div>
+								<div class="form-block">
+									<input type="email" name="email" class="form-control"placeholder="Email">
+									
+								</div>
+								<div class="form-block">
+									<textarea name="mensaje" placeholder="Mensaje"></textarea>
+								</div>
+								<div class="form-block bloque-ultimo">
+									<input type="submit" class="boton boton-negro" value="Enviar">
+									
+								</div>
+                               <?php
+
+									if($_SERVER["REQUEST_METHOD"] == "POST")
+									{
+										$nombre = $_POST["nombre"] ;
+										$email = $_POST["email"] ;
+										$mensaje = $_POST["mensaje"] ;
+
+										if(isset($nombre))
+										{
+											if(isset($email))
+											{
+												if(isset($mensaje))
+												{
+													$para = "joseeduardogonzalezmendoza1@gmail.com"; //colocar aqui el correo a dodne quieres que llegue
+													$asunto = "Esto es una prueba";
+													$cuerpo = $nombre."\n".$email."\n".$mensaje;
+													$adicional = "From: noreply@jacoboamaru.com";
+
+													mail($para,$asunto,$cuerpo,$adicional);
+												?>
+													<p>Envio exitoso</p>
+												<?php
+												}
+											}
+										}
+									}
+								?>
+
+							</form>
+						</div>	
+					</div>	
+				</div>
+				
+			</section>
+		</main>
+		<footer class="seccion">
+			<div class="container">
+				<div class="row">
+					<div class="columna columna-25 columna-mobile-100">
+						<img src="img/logo-blanco.png" class="logo-footer">
+						<p>
+						Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Ut tincidunt posuere condimentum. Quisque pellentesque ultricies turpis, a efficitur lacus lacinia quis. Sed accumsan dignissim facilisis.
+						</p>
+
+					</div>
+					<div class="columna columna-25 columna-mobile-100">
+						<h3>
+							Temas relacionados
+						</h3>
+						<ul>
+							<li><a href="#">Tema 1</a></li>
+							<li><a href="#">Tema 2</a></li>
+							<li><a href="#">Tema 3</a></li>
+						</ul>
+					</div>
+					<div class="columna columna-25 columna-mobile-100">
+						<h3>
+							Datos de contacto
+						</h3>
+						<ul>
+							<li>joselalo-1@live.com</li>
+							<li>55-26-98-83-26</li>
+							<li>Xochimil, Pblo Santa Cruz Acapixca<br>Ciudad de mexico</li>
+						</ul>
+					</div>
+					<div class="columna columna-25 columna-mobile-100">
+						<h3>
+							Redes sociales
+						</h3>
+						<ul class="redes">
+							<li><a href="#"><i class="fab fa-facebook"></i></i></a></li>
+							<li><a href="#"><i class="fab fa-twitter"></i></i></a></li>
+							<li><a href="#"><i class="fab fa-instagram"></i></i></a></li>
+						</ul>
+					</div>
+	
+					
+				</div>
+				
+			</div>
+			<div class="barra-footer">
+				&copy;Derechos Reservados - 2020
+				
+			</div>
+		</footer>
+		<script src="js/jquery.js"></script>
+		<script src="js/funciones.js"></script>
+	</body>
+</html>
